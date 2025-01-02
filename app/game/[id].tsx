525f628d37740e5e9dfda331a2db0199eed79d1c
Eleni001 <Eleni.SolomonSamuelsson@medieinstitutet.se>
2025-01-03 00:22:49 +0100

    add the barn image and animate it

--- app/game/[id].tsx
+++ app/game/[id].tsx
@@ -1,30 +1,61 @@
 import BackgroundImage from "@/components/BackgroundImage";
 import { themes } from "@/constants/themes";
 import { useLocalSearchParams } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect } from "react";
+import { Dimensions, Image, StyleSheet } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withRepeat,
+  withSequence,
+  withTiming,
+} from "react-native-reanimated";
 
 export default function Game() {
   const { id } = useLocalSearchParams();
   const theme = themes.find((t) => t.id === id);
+  const rotationAnimation = useSharedValue(0);
+
+  useEffect(() => {
+    rotationAnimation.value = withRepeat(
+      withSequence(
+        withTiming(10, { duration: 300 }),
+        withTiming(0, { duration: 300 }),
+        withTiming(10, { duration: 300 }),
+        withTiming(0, { duration: 300 }),
+        withTiming(10, { duration: 300 }),
+        withTiming(0, { duration: 300 }),
+        withDelay(500, withTiming(0, { duration: 0 }))
+      ),
+      500
+    );
+  }, []);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ rotate: `${rotationAnimation.value}deg` }],
+  }));
+
   return (
     <BackgroundImage source={theme?.background}>
-      <View style={styles.container}>
-        <Text style={styles.title}>{theme?.title}</Text>
-      </View>
+      <Animated.View style={animatedStyle}>
+        <Image
+          source={theme?.cover}
+          style={styles.cover}
+          resizeMode="contain"
+        />
+      </Animated.View>
     </BackgroundImage>
   );
 }
 
+const { width, height } = Dimensions.get("window");
+
 const styles = StyleSheet.create({
-  container: {
-    justifyContent: "center",
-    alignItems: "center",
-    gap: 0,
-  },
-  title: {
-    fontSize: 76,
-    fontWeight: "bold",
-    margin: 10,
-    color: "red",
+  cover: {
+    flex: 1,
+    marginTop: height * 0.23,
+    width: width * 0.6,
+    height: height * 0.6,
   },
 });
--- constants/themes.ts
+++ constants/themes.ts
@@ -4,17 +4,20 @@ export const themes = [
     title: "Farm",
     image: require("../assets/images/farm.png"),
     background: require("../assets/images/background1.png"),
+    cover: require("../assets/images/barn.png"),
   },
   {
     id: "2",
     title: "Colors",
     image: require("../assets/images/colors.png"),
     background: require("../assets/images/colors.png"),
+    cover: require("../assets/images/background1.png"),
   },
   {
     id: "3",
     title: "Kitchen",
     image: require("../assets/images/kitchen.png"),
     background: require("../assets/images/colors.png"),
+    cover: require("../assets/images/background1.png"),
   },
 ];
